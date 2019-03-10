da38a00817ac51e62792710d5921a8be4f409de6
cutegony <cutegony77@gmail.com>
2019-03-09 18:29:50 -0800

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -28,13 +28,13 @@ public class BubbleSort {
         int[] sortedArray = {1,2,3,4,5,6};
 
         int[] testArray1 = {6,2,3,5,1,4};
-        System.out.println("input1:" + Arrays.toString(testArray1) + "\n");
+        System.out.println("input1:" + Arrays.toString(testArray1));
         int[] resultArray1 = getBubbleSort(testArray1);
         System.out.println("result1:" + Arrays.toString(resultArray1) + "\n");
         Assert.assertArrayEquals(sortedArray,resultArray1);
 
         int[] testArray2 = {3,5,2,4,6,1};
-        System.out.println("input1:" + Arrays.toString(testArray2) + "\n");
+        System.out.println("input1:" + Arrays.toString(testArray2));
         int[] resultArray2 = getBubbleSort(testArray2);
         System.out.println("result2:" + Arrays.toString(resultArray2));
         Assert.assertArrayEquals(sortedArray,resultArray2);
