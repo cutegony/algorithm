e08b01c90e39c7bb42bf41fc8442afca3d7c0b25
cutegony <cutegony77@gmail.com>
2019-03-11 23:12:13 -0700

    Create Stack.java

---
+++ Stack.java
@@ -0,0 +1,154 @@
+package algorithm;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Stack<Item> implements Iterable<Item> {
+    private int max = 5;
+    private int n;          // size of the stack
+    private Node first;     // top of stack
+
+    // helper linked list class
+    private class Node {
+        private Item item;
+        private Node next;
+    }
+
+    /**
+     * Initializes an empty stack.
+     */
+    public Stack() {
+        first = null;
+        n = 0;
+    }
+
+    /**
+     * Returns true if this stack is empty.
+     *
+     * @return true if this stack is empty; false otherwise
+     */
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    /**
+     * Returns the number of items in this stack.
+     *
+     * @return the number of items in this stack
+     */
+    public int size() {
+        return n;
+    }
+
+    /**
+     * Adds the item to this stack.
+     *
+     * @param  item the item to add
+     * @throws StackOverflowError if this stack is empty
+     */
+    public void push(Item item) {
+        if(n <= max) {
+            Node oldfirst = first;
+            first = new Node();
+            first.item = item;
+            first.next = oldfirst;
+            n++;
+        } else {
+            throw new StackOverflowError("Stack overflow");
+        }
+    }
+
+    /**
+     * Removes and returns the item most recently added to this stack.
+     *
+     * @return the item most recently added
+     * @throws NoSuchElementException if this stack is empty
+     */
+    public Item pop() {
+        if (isEmpty()) throw new NoSuchElementException("Stack underflow");
+        Item item = first.item;        // save item to return
+        first = first.next;            // delete first node
+        n--;
+        return item;                   // return the saved item
+    }
+
+
+    /**
+     * Returns (but does not remove) the item most recently added to this stack.
+     *
+     * @return the item most recently added to this stack
+     * @throws NoSuchElementException if this stack is empty
+     */
+    public Item peek() {
+        if (isEmpty()) throw new NoSuchElementException("Stack underflow");
+        return first.item;
+    }
+
+    /**
+     * Returns a string representation of this stack.
+     *
+     * @return the sequence of items in this stack in LIFO order, separated by spaces
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Item item : this) {
+            s.append(item);
+            s.append(' ');
+        }
+        return s.toString();
+    }
+
+
+    /**
+     * Returns an iterator to this stack that iterates through the items in LIFO order.
+     *
+     * @return an iterator to this stack that iterates through the items in LIFO order
+     */
+    public Iterator<Item> iterator()  { return new ListIterator();  }
+
+    // an iterator, doesn't implement remove() since it's optional
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+        public boolean hasNext()  { return current != null;                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+    @Test
+    public void testStack() {
+        Stack stack = new Stack();
+        Assert.assertTrue(stack.isEmpty());
+        Assert.assertEquals(stack.size(), 0);
+
+        stack.push(1);
+        stack.push(2);
+        stack.toString();
+        stack.pop();
+        stack.toString();
+        stack.push(3);
+        Assert.assertEquals(stack.peek(), 3);
+        Assert.assertFalse(stack.isEmpty());
+        Assert.assertEquals(stack.size(), 2);
+
+        stack.pop();
+        stack.pop();
+        //stack.pop();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        //stack.push(7);
+    }
+}
