3e6ed8996f26cd00452916dd21b9c5e68eb3a894
Joseph Moon <cutegony37@gmail.com>
2019-06-24 22:29:27 -0700

    added unittest.main()

--- python/test_01_bubble_sort.py
+++ python/test_01_bubble_sort.py
@@ -8,8 +8,11 @@ def bubblesort(alist):
     return alist
 
 
-class unit_test(unittest.TestCase):
+class Test01BubbleSort(unittest.TestCase):
     def test(self):
         self.assertEqual([1, 2, 3, 4, 5, 6], bubblesort([4, 6, 1, 3, 5, 2]))
         self.assertEqual([1, 2, 3, 4, 5, 6], bubblesort([6, 4, 3, 1, 2, 5]))
         self.assertEqual([1, 2, 3, 4, 5, 6], bubblesort([6, 5, 4, 3, 2, 1]))
+
+if __name__ == '__main__':
+    unittest.main()
